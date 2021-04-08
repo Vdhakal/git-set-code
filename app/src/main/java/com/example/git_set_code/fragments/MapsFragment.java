0082c1e495844ce4c5ac10a2dcca95ebb4bddb66
Vaskar Dhakal <30727785+Vdhakal@users.noreply.github.com>
2021-04-08 00:13:19 -0500

    map_inflated

--- app/src/main/java/com/example/git_set_code/fragments/MapsFragment.java
+++ app/src/main/java/com/example/git_set_code/fragments/MapsFragment.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +17,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapScene;
+import com.here.sdk.mapview.MapScheme;
+import com.here.sdk.mapview.MapView;
 
 public class MapsFragment extends Fragment {
+
+    private static final String TAG = "Maps";
+    private MapView mapView;
+    private View rootView;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -33,26 +43,66 @@ public class MapsFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     };
+
+    public MapsFragment() {
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        rootView =  inflater.inflate(R.layout.fragment_maps, container, false);
+        return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+//        SupportMapFragment mapFragment =
+//                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapView = rootView.findViewById(R.id.map_view);
+
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+            //ASK FOR PERMISSIONS
+
+            loadMapScene();
         }
+    }
+    private void loadMapScene() {
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
+            @Override
+            public void onLoadScene(@Nullable MapError mapError) {
+                if (mapError == null) {
+                    double distanceInMeters = 1000 * 1.5;
+                    mapView.getCamera().lookAt(
+                            new GeoCoordinates(32.53193134803399, -92.07568698554728), distanceInMeters);
+                } else {
+                    Log.d(TAG, "Loading map failed: mapError: " + mapError.name());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
