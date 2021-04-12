7620fdeb786ee3feefa9326deb724b6f0fb34142
Vaskar Dhakal <30727785+Vdhakal@users.noreply.github.com>
2021-04-12 04:38:07 -0500

    slider

--- app/src/main/java/com/example/git_set_code/adapters/TripsAdapter.java
+++ app/src/main/java/com/example/git_set_code/adapters/TripsAdapter.java
@@ -1,10 +1,15 @@
 package com.example.git_set_code.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -18,6 +23,8 @@ import com.example.git_set_code.viewmodels.TripsData;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ng.max.slideview.SlideView;
 
 public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> {
 
@@ -40,12 +47,34 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull TripsAdapter.ViewHolder holder, int position) {
+        holder.getExpandableLayout().setVisibility(expanded ? View.VISIBLE : View.GONE);
+        holder.getTitle().setText("Trip 1");
         holder.getProductName().setText(tripsDataList.get(1).getProductDesc());
         holder.getStops().setText(String.valueOf(tripsDataList.get(0).getStops()));
+        setUpStepView(holder);
+        setUpSlider(holder);
+    }
 
+    private void setUpSlider(ViewHolder holder) {
+        holder.getSlideView().setOnSlideCompleteListener(new SlideView.OnSlideCompleteListener() {
+            @Override
+            public void onSlideComplete(SlideView slideView) {
+                // vibrate the device
+                Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                if(vibrator.hasVibrator()){vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE)); }
+                holder.getSlideView().setText("Selected");
+                holder.getSlideView().setButtonBackgroundColor(ColorStateList.valueOf(Color.LTGRAY));
+                holder.getSlideView().setSlideBackgroundColor(ColorStateList.valueOf(Color.GRAY));
+                holder.getSlideView().setEnabled(false);
+
+            }
+        });
+    }
+
+    private void setUpStepView(ViewHolder holder) {
         List<String> stepsBeanList = new ArrayList<>();
         for (int i=0; i<tripsDataList.size(); i++){
-            stepsBeanList.add(tripsDataList.get(i).getDestinationName()+"\n"+tripsDataList.get(i).getAddress1()+" "+tripsDataList.get(i).getCity().trim()+" "+tripsDataList.get(i).getStateAbbrev());
+            stepsBeanList.add(tripsDataList.get(i).getDestinationName().trim().toUpperCase()+"\n"+tripsDataList.get(i).getAddress1().trim().toUpperCase()+" "+tripsDataList.get(i).getCity().trim().toUpperCase()+" "+tripsDataList.get(i).getStateAbbrev().trim().toUpperCase());
         }
         stepsBeanList.add("");
         holder.getStepView().setStepsViewIndicatorComplectingPosition(stepsBeanList.size()-1)
@@ -57,10 +86,8 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> 
                 .setStepViewComplectedTextColor(ContextCompat.getColor(context, R.color.step_indicator))
                 .setStepViewUnComplectedTextColor(ContextCompat.getColor(context,  R.color.background_orange))
                 .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(context, R.drawable.source_circle))
-                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(context, R.drawable.source_circle))
-                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(context, R.drawable.source_circle)).setTextSize(16);
-
-        holder.getExpandableLayout().setVisibility(expanded ? View.VISIBLE : View.GONE);
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(context, R.drawable.ic_baseline_check_circle_24))
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(context, R.drawable.ic_baseline_check_circle_24)).setTextSize(16);
     }
 
     @Override
@@ -68,28 +95,35 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> 
         return 1;
     }
     public class ViewHolder extends RecyclerView.ViewHolder{
-        private final TextView  productName, stops;
+        private final TextView  productName, stops, title;
         private final CardView cardView;
         private final ConstraintLayout expandableLayout;
         private final VerticalStepView stepView;
+        private final SlideView slideView;
 
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.trips_card_view);
+            title = itemView.findViewById(R.id.trip_title);
             productName = itemView.findViewById(R.id.tv_product_name);
             stops = itemView.findViewById(R.id.tv_stops);
             expandableLayout = itemView.findViewById(R.id.expandable_layout);
             stepView = (VerticalStepView) itemView.findViewById(R.id.step_view);
-
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    expanded = !expanded;
-                    notifyItemChanged(getAdapterPosition());
-                }
-            });
+            slideView = (SlideView) itemView.findViewById(R.id.slideView);
+            expandOnClick(cardView);
         }
+
+        private void expandOnClick(CardView cardView) {
+            cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expanded = !expanded;
+                notifyItemChanged(getAdapterPosition());
+            }
+        });
+        }
+
         public TextView getProductName() { return productName; }
 
         public TextView getStops() {
@@ -102,5 +136,12 @@ public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> 
             return stepView;
         }
 
+        public SlideView getSlideView() {
+            return slideView;
+        }
+
+        public TextView getTitle() {
+            return title;
+        }
     }
 }
