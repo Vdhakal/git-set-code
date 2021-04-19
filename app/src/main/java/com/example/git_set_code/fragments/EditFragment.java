6bbc34397e45dda70a0745c33ec342c2eca12063
Prativa Karki <prativakarki96@gmail.com>
2021-04-19 01:02:44 -0500

    captured signature and saved in bitmap

--- app/src/main/java/com/example/git_set_code/fragments/EditFragment.java
+++ app/src/main/java/com/example/git_set_code/fragments/EditFragment.java
@@ -1,14 +1,20 @@
 package com.example.git_set_code.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.git_set_code.R;
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +27,8 @@ public class EditFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private SignaturePad signaturePad = null;
+    private Bitmap signatureBitmap = null;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -62,5 +69,28 @@ public class EditFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.edit_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        signaturePad = getView().findViewById(R.id.signaturePad1);
+        signaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+
+            }
+
+            @Override
+            public void onSigned() {
+                //signature saved here
+                signatureBitmap = signaturePad.getSignatureBitmap();
+            }
+
+            @Override
+            public void onClear() {
+
+            }
+        });
     }
 }
